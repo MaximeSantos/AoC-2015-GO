52323837d912c88fab04e31824159bae58833fc4
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2023-10-07 16:56:10 +0200

    Add template file for main.go

---
+++ template/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func part1() {
+	fmt.Println("Part 1")
+	fmt.Println("Hello")
+}
+
+func part2() {
+	fmt.Println("----------------")
+	fmt.Println("Part 2")
+	fmt.Println("World")
+}
+
+func main() {
+	part1()
+	part2()
+
+}
